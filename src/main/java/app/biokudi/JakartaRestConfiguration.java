cec6217797b7ed6c874dc97fe10eb2102008f121
Tnorvy <gabrielemartinez@ucundinamarca.edu.co>
2023-04-21 19:59:54 -0500

    First Commit

---
+++ src/main/java/app/biokudi/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package app.biokudi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
